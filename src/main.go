0f9e89ec360bf02e9d190cca4da4660726837eab
Balin Warren <balin.warren@gmail.com>
2024-06-16 16:04:59 -0700

    Added all driver per year endpoint to api

--- src/main.go
+++ src/main.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gofor-little/env"
 	"go.mongodb.org/mongo-driver/bson"
+
+	//"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+/*type Driver struct {
+	_id         primitive.ObjectID `json:"_id"`
+	firstName   string	`json:"first_name"`
+	lastName    string	`json:"last_name"`
+	dateOfBirth string	`json:"date_of_birth"`
+	dateofDeath string	`json:"date_of_death"`
+	nationality string
+	teamsRaced  []string
+	yearsActive []int
+	raceStarts  int
+	wdc         int
+	wins        int
+	podiums     int
+	points      int
+	uniqueGpWon []string
+	active      bool
+	carNumber   string
+}*/
 
 func main() {
 	//load env
@@ -64,7 +87,39 @@ func main() {
 		fmt.Fprintf(w, "Return all drivers\n")
 		fmt.Println("Endpoint hit: all drivers")
 		for _, driver := range drivers {
-			fmt.Fprintf(w, "%v\n\n", driver)
+			jsonBytes, err := json.MarshalIndent(driver, "", "   ")
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Fprintf(w, "%s\n\n", string(jsonBytes))
+		}
+	})
+
+	//all drivers by year endpoint
+	mux.HandleFunc("GET /{year}/drivers", func(w http.ResponseWriter, r *http.Request) {
+		year, err := strconv.Atoi(r.PathValue("year"))
+		if err != nil {
+			panic(err)
+		}
+
+		cursor, err := driverCollection.Find(context.TODO(), bson.M{"yearsActive": year})
+		if err != nil {
+			panic(err)
+		}
+		var drivers []bson.M
+		if err = cursor.All(context.TODO(), &drivers); err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "Return all drivers active in year %v\n", year)
+		fmt.Printf("Endpoint hit: all drivers in year %v\n", year)
+		for _, driver := range drivers {
+			jsonBytes, err := json.MarshalIndent(driver, "", "   ")
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Fprintf(w, "%s\n\n", string(jsonBytes))
 		}
 	})
 
